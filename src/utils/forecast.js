e4f8f9aeb1e918c06249cbdb154c1d543e5ffedc
Rohit Goyal <goyal781@gmail.com>
2021-01-21 13:55:12 +0530

    add forecast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude,longitude,callback) =>
 {
-    const url = 'http://api.weatherstack.com/current?access_key=c99e137bd79b26eee560308a705eb647&query='+ latitude + ',' + longitude +'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=c99e137bd79b26eee560308a705eb647&query='+ latitude + ',' + longitude +'&units=m'
   request({url, json: true },(error,{body}) => {
    if(error){
        callback('Unable to connect to Weather services',undefined)
@@ -12,7 +12,7 @@ const forecast = (latitude,longitude,callback) =>
        callback('unable to find location',undefined)
    }
    else{
-       callback(undefined,body.current.weather_descriptions+'. It is currently ' +body.current.temperature + ' degree out . but it feels like '+body.current.feelslike
+       callback(undefined,body.current.weather_descriptions+'. It is currently ' +body.current.temperature + ' degree out . But it feels like '+body.current.feelslike+'.It feels like there is '+body.current.precip+'% chances of rain today. '
             
        )
    }
